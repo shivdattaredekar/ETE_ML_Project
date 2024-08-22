0ad57741104be11847ac776554701e157b434d4e
Shivdatta redekar <shivdattaredekar@gmail.com>
2024-08-22 16:16:59 +0530

    Streamlit app developed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import os 
+
+# Importing necessary libraries for the project
 import numpy as np
-import pandas as pd
 from Mlproject.pipeline.prediction import PredictionPipeline
 import streamlit as st
 import joblib
@@ -10,7 +10,6 @@ st.title('Welcome to Default Prediction')
 st.sidebar.header('Please enter your values here')
 
 # Collecting all slider inputs with appropriate types
-LIMIT_BAL = float(st.sidebar.slider('LIMIT_BAL:', min_value=0.0, max_value=1000000.0, value=50000.0))
 SEX = int(st.sidebar.slider('SEX:', min_value=1, max_value=2, value=1))
 EDUCATION = int(st.sidebar.slider('EDUCATION:', min_value=0, max_value=6, value=6))
 MARRIAGE = int(st.sidebar.slider('MARRIAGE:', min_value=0, max_value=3, value=1))
@@ -21,18 +20,19 @@ PAY_3 = int(st.sidebar.slider('PAY_3:', min_value=-2, max_value=8, value=0))
 PAY_4 = int(st.sidebar.slider('PAY_4:', min_value=-2, max_value=8, value=0))
 PAY_5 = int(st.sidebar.slider('PAY_5:', min_value=-2, max_value=8, value=0))
 PAY_6 = int(st.sidebar.slider('PAY_6:', min_value=-2, max_value=8, value=0))
-BILL_AMT1 = float(st.sidebar.slider('BILL_AMT1:', min_value=0.0, max_value=1000000.0, value=50000.0))
-BILL_AMT2 = float(st.sidebar.slider('BILL_AMT2:', min_value=0.0, max_value=1000000.0, value=50000.0))
-BILL_AMT3 = float(st.sidebar.slider('BILL_AMT3:', min_value=0.0, max_value=1000000.0, value=50000.0))
-BILL_AMT4 = float(st.sidebar.slider('BILL_AMT4:', min_value=0.0, max_value=1000000.0, value=50000.0))
-BILL_AMT5 = float(st.sidebar.slider('BILL_AMT5:', min_value=0.0, max_value=1000000.0, value=50000.0))
-BILL_AMT6 = float(st.sidebar.slider('BILL_AMT6:', min_value=0.0, max_value=1000000.0, value=50000.0))
-PAY_AMT1 = float(st.sidebar.slider('PAY_AMT1:', min_value=0.0, max_value=1000000.0, value=5000.0))
-PAY_AMT2 = float(st.sidebar.slider('PAY_AMT2:', min_value=0.0, max_value=1000000.0, value=5000.0))
-PAY_AMT3 = float(st.sidebar.slider('PAY_AMT3:', min_value=0.0, max_value=1000000.0, value=5000.0))
-PAY_AMT4 = float(st.sidebar.slider('PAY_AMT4:', min_value=0.0, max_value=1000000.0, value=5000.0))
-PAY_AMT5 = float(st.sidebar.slider('PAY_AMT5:', min_value=0.0, max_value=1000000.0, value=5000.0))
-PAY_AMT6 = float(st.sidebar.slider('PAY_AMT6:', min_value=0.0, max_value=1000000.0, value=5000.0))
+LIMIT_BAL = float(st.sidebar.number_input(label = 'LIMIT_BAL'))
+BILL_AMT1 = float(st.sidebar.number_input(label = 'BILL_AMT1'))
+BILL_AMT2 = float(st.sidebar.number_input(label = 'BILL_AMT2'))
+BILL_AMT3 = float(st.sidebar.number_input(label = 'BILL_AMT3'))
+BILL_AMT4 = float(st.sidebar.number_input(label = 'BILL_AMT4'))
+BILL_AMT5 = float(st.sidebar.number_input(label = 'BILL_AMT5'))
+BILL_AMT6 = float(st.sidebar.number_input(label = 'BILL_AMT6'))
+PAY_AMT1 = float(st.sidebar.number_input(label = 'PAY_AMT1'))
+PAY_AMT2 = float(st.sidebar.number_input(label = 'PAY_AMT2'))
+PAY_AMT3 = float(st.sidebar.number_input(label = 'PAY_AMT3'))
+PAY_AMT4 = float(st.sidebar.number_input(label = 'PAY_AMT4'))
+PAY_AMT5 = float(st.sidebar.number_input(label = 'PAY_AMT5'))
+PAY_AMT6 = float(st.sidebar.number_input(label = 'PAY_AMT6'))
 
 # Use the inputs in your prediction pipeline, if applicable
 if st.sidebar.button('Predict'):
@@ -50,8 +50,15 @@ if st.sidebar.button('Predict'):
         
         prediction_pipeline = PredictionPipeline()
         result = prediction_pipeline.predict(data)
-        
-        st.write(f'Prediction result: {result}')
+        response = '' 
+        if result == 1:
+            response = 'The client is likely to default.'
+        else :
+            response = 'The client is not likely to default.'
+         
+
+
+        st.write(f'Prediction result: {response}')
     except Exception as e:
         st.write(f'An error occurred: {e}')
         
